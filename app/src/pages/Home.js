91cb17841a65a84c069bf30a2547f2adee9cd7aa
PCguyNoah <noahbeightol14@gmail.com>
2024-10-04 22:21:05 -0400

    build out homepage

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import './Home.css';
 import AppBar from '@mui/material/AppBar';
 import headshot from '../images/headshot.png'
@@ -6,13 +7,27 @@ const Home = () => {
   document.title = '| Noah Beightol'
 
 
-    return (
-        <div>
-          <div className='main-container'>
-            <div className='main-banner'></div>
+  return (
+      <div>
+        <div className='main-container'>
+          <div className='main-banner'>
+            <div className='split-first-banner'>
+              <div className='branded-title'>Noah Beightol</div>
+            </div>
+
+            <div className='split-last-banner'>
+              <button className='branded-title-button'>About</button>
+              <button className='branded-title-button'>Contact</button>
+            </div>
           </div>
         </div>
-    );
+        <div className='main-content-container'>
+           <div className='card'></div>
+           <div className='card2'></div>
+           <div className='card3'></div>
+        </div>
+      </div>
+  );
 }
 
 export default Home;
