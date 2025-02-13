0cccd966aa83cc5645c1d9d39bfaba84e157c0e0
PCguyNoah <noahbeightol14@gmail.com>
2025-02-13 00:12:15 -0500

    Refactor

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,44 +1,68 @@
 import React, { useState, useEffect } from 'react';
 import './Home.css';
-import AppBar from '@mui/material/AppBar';
-import Avatar from '@mui/material/Avatar';
-import headshot from '../images/DSG_Interns-0178-Edit.jpg'
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+import headshot from '../images/file.png'
+import Typed from 'typed.js';
 
 const Home = () => {
   document.title = '| Noah Beightol'
 
+  const el = React.useRef(null);
+  React.useEffect(() =>
+  {
+      const typed = new Typed(el.current, {
+          strings: ['Hello^1000', 'Hola^1000', 'Bonjour^1000', 'Nǐn hǎo^1000','Ciao^1000'],
+          smartBackspace: true,
+          shuffle: true,
+          typeSpeed: 70,
+      });
 
+      return () =>
+      {
+          // Destroy Typed instance during cleanup to stop animation
+          typed.destroy();
+      };
+  }, []);
+
+  function handleClick(event) {
+    event.preventDefault();
+    console.info('You clicked a breadcrumb.');
+  }
   return (
       <div>
         <div className='main-container'>
-          <div className='main-banner'>
-            <div className='split-first-banner'>
-              <div className='branded-title'>Noah Beightol</div>
+          <div className='content-container'>
+          <span className="home-title" ref={el} />
+            <div className='card'>
             </div>
+          </div>
 
-            <div className='split-last-banner'>
-              <button className='branded-title-button'>About</button>
-              <button className='branded-title-button'>Contact</button>
-            </div>
+          
+        
+
+          <div className='bottom-container' role="presentation" onClick={handleClick}>
+            <Breadcrumbs className='bottom-container-text' aria-label="breadcrumb">
+              <Link underline="hover" color="text.primary" href="/">
+                About
+              </Link>
+              <Link
+                underline="hover"
+                color="inherit"
+                href="/material-ui/getting-started/installation/"
+              >
+                Experience
+              </Link>
+              <Link
+                underline="hover"
+                color="inherit"
+                href="/material-ui/react-breadcrumbs/"
+                aria-current="page"
+              >
+                Contact
+              </Link>
+            </Breadcrumbs>
           </div>
-        </div>
-        <div className='main-content-container'>
-          <div className='card3'>
-            Hello
-          </div>
-          <div className='card'></div>
-          <div className='card2 '>
-            <img 
-              className='image'
-              alt="Noah Beightol"
-              src={headshot}
-            />
-          </div>
-        </div>
-        <div className='main-content-container'>
-           <div className='card'></div>
-           <div className='card2'></div>
-           <div className='card3'></div>
         </div>
       </div>
   );
